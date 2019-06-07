975ffd27e8141ab32e37bb21c2cc4a71677f04b8
Hadas <bekihs2003@gmail.com>
2019-06-07 09:42:33 +0300

    add space

--- testing/sel.py
+++ testing/sel.py
@@ -1,7 +1,7 @@
 # setting up chrome browser on a desired page
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By    
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(executable_path="C:\\chromedriver_win32\\chromedriver.exe")
 # opening chrome browser on a desired page
