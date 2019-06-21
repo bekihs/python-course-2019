8d034e8a45f75502f7fe1baee5cecba7c44598ea
Hadas <bekihs2003@gmail.com>
2019-06-21 10:50:23 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -1,11 +1,41 @@
 from flask import *
-from flask import request
-
-app = Flask(__name__, template_folder="templates")
+from flask import request , render_template
+import json
+f = open("users.json" , "w")
+f.close()
+app = Flask(__name__, template_folder="static")
  
 @app.route("/")
 def index():
-    return "hi there, this is the best web site in the world"
+    return render_template("1.html" , name="world")
+@app.route("/<name>")
+def my_name(name):
+    return render_template("1.html" , name=name)
+
+@app.route("/message")
+def get_messages():
+    f = open("users.json" , "r")
+    lines = f.readlines()
+    return jsonify(lines)
+@app.route("/message" , methods=['post'])
+def new_message():
+    print(json.dumps(request.form))
+    f = open("users.json" , "a")
+    f.writelines([ json.dumps(request.form) , ","])
+    f.close()
+    #save data in database 
+    return "yessss" + request.form["text"]
+
+
+@app.route("/user" , methods=['put'])
+def update_user():
+    return "this is an updae"
+@app.route("/user/<user_id>" , methods=['get'])
+def get_user(user_id):
+    return "this is a get"
+@app.route("/user/<user_id>" , methods=['delete'])
+def delete_user(user_id):
+    return "this is a delete"
 
 @app.route("/<username>/<int:age>")
 def user(username,age):
